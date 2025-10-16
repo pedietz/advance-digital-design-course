72fce784d06e7c1ae33e720fb3d2bb89a254e5b7
Pablo Dietz <pablo.e.dietz@gmail.com>
2025-10-16 17:12:58 +0200

    Se añadieron nuevos metodos de recondeo y se corrigió el metodo wrap que no estaba funcionando de forma adecuada.

--- lib/fixp.py
+++ lib/fixp.py
@@ -4,61 +4,75 @@ from numbers import Real
 from typing import Callable
 import math
 
-# Saturation strategies:
+# Overflow actions:
 
 @staticmethod
 def saturate(value: int, limits: tuple[int, int]) -> int:
-    min_value, max_value = limits
+    min_value, max_value = (min(limits), max(limits))
     return max_value if value > max_value else min_value if value < min_value else value
 
 @staticmethod
 def wrap(value: int, limits: tuple[int, int]) -> int:
-    min_value, max_value = limits
-    return value - max_value + min_value - 1 if value > max_value else \
-           value + max_value - min_value + 1 if value < min_value else value
+    min_value, max_value = (min(limits), max(limits))
+    return (value - min_value) % (max_value - min_value + 1) + min_value
 
-# Rounding strategies:
+# Rounding methods:
 
 @staticmethod
 def truncate(value: Real) -> int:
     return math.floor(value) if value < 0 else int(value)
 
 @staticmethod
+def round_up(value: Real) -> int:
+    return math.ceil(value)
+
+@staticmethod
+def round_dw(value: Real) -> int:
+    return math.floor(value)
+
+@staticmethod
 def round_half_up(value: Real) -> int:
-    frac = math.modf(value)[0]
-    return math.ceil(value) if math.isclose(abs(frac), 0.5, rel_tol=1e-18) else round(value)
+    return math.floor(value + 0.5)
 
 @staticmethod
 def round_half_dw(value: Real) -> int:
-    frac = math.modf(value)[0]
-    return math.floor(value) if math.isclose(abs(frac), 0.5, rel_tol=1e-18) else round(value)
+    return math.ceil(value - 0.5)
 
 @staticmethod
-def round_to_even(value: Real) -> int:
+def round_half_toward_zero(value: Real) -> int:
+    return math.ceil(value - 0.5) if value >= 0 else math.floor(value + 0.5)
+
+@staticmethod
+def round_half_away_from_zero(value: Real) -> int:
+    return math.floor(value + 0.5) if value >= 0 else math.ceil(value - 0.5)
+
+@staticmethod
+def round_half_to_even(value: Real) -> int:
     return round(value)
 
 @staticmethod
-def round_to_odd(value: Real) -> int:
-    return round(value)
+def round_half_to_odd(value: Real) -> int:
+    return math.floor(value + 0.5) - int(value) % 2 if value >= 0 else \
+           math.ceil(value - 0.5) + int(value) % 2
 
 # DeFixedInt Class:
 
 @dataclass
 class DeFixedInt:
 
-    init_value:    Real = 0
-    int_width:     int  = 0
-    frac_width:    int  = 15
-    signed_mode:   str  = 'S'
-    round_func:    Callable[[Real], int] = field(default=truncate)
-    saturate_func: Callable[[int, tuple[int, int]], int] = field(default=saturate)
+    init_value:      Real = 0
+    int_width:       int  = 0
+    frac_width:      int  = 15
+    signedness:      str  = 'S'
+    rounding_method: Callable[[Real], int] = field(default=truncate)
+    overflow_action: Callable[[int, tuple[int, int]], int] = field(default=saturate)
 
     def __post_init__(self):
         if self.int_width < 0 and abs(self.int_width) >= self.frac_width:
             raise ValueError("Integer width needs to be >= -(fractional width)!")
         if self.frac_width < 0:
             raise ValueError("Fractional width needs to be >= 0!")
-        if self.signed_mode not in {'S', 'U'}:
+        if self.signedness not in {'S', 'U'}:
             raise ValueError(f"Signed mode '{self.signed_mode}' not supported!")
         self.value = self.init_value
     
@@ -69,8 +83,8 @@ class DeFixedInt:
     @value.setter
     def value(self, value) -> None:
         aux = value * (2.0 ** self.frac_width)
-        aux = self.round_func(aux)
-        self.__value = self.saturate_func(aux, self.range)
+        aux = self.rounding_method(aux)
+        self.__value = self.overflow_action(aux, self.range)
 
     @property
     def fvalue(self) -> float:
@@ -82,12 +96,12 @@ class DeFixedInt:
 
     @property
     def range(self) -> tuple[int, int]:
-        max_lim =  2 ** (self.width - int(self.signed_mode == 'S')) - 1
-        min_lim = -2 ** (self.width - 1) * int(self.signed_mode == 'S')
+        max_lim =  2 ** (self.width - int(self.signedness == 'S')) - 1
+        min_lim = -2 ** (self.width - 1) * int(self.signedness == 'S')
         return (min_lim, max_lim)
     
     def __repr__(self) -> str:
-        return f"<{self.value} ({self.fvalue:.3f}) {self.signed_mode}({self.int_width + self.frac_width},{self.frac_width})>"
+        return f"<{self.value} ({self.fvalue:.3f}) {self.signedness}({self.int_width + self.frac_width},{self.frac_width})>"
 
     def __index__(self) -> int:
         return self.value
@@ -108,36 +122,33 @@ class DeFixedInt:
         return self.fvalue >= other.fvalue
 
     def __add__(self, other: DeFixedInt) -> DeFixedInt:
-        ret = DeFixedInt(int_width     = max(self.int_width, other.int_width) + 1,
-                         frac_width    = max(self.frac_width, other.frac_width),
-                         signed_mode   = self.signed_mode,
-                         round_func    = self.round_func,
-                         saturate_func = self.saturate_func)
-        ret.value = self.fvalue + other.fvalue
-        return ret
+        return DeFixedInt(init_value      = self.fvalue + other.fvalue,
+                          int_width       = max(self.int_width,  other.int_width) + 1,
+                          frac_width      = max(self.frac_width, other.frac_width),
+                          signedness      = self.signedness,
+                          rounding_method = self.rounding_method,
+                          overflow_action = self.overflow_action)
     
     def __sub__(self, other: DeFixedInt) -> DeFixedInt:
-        ret = DeFixedInt(int_width     = max(self.int_width, other.int_width) + 1,
-                         frac_width    = max(self.frac_width, other.frac_width),
-                         signed_mode   = self.signed_mode,
-                         round_func    = self.round_func,
-                         saturate_func = self.saturate_func)
-        ret.value = self.fvalue - other.fvalue
-        return ret
+        return DeFixedInt(init_value      = self.fvalue - other.fvalue,
+                          int_width       = max(self.int_width, other.int_width) + 1,
+                          frac_width      = max(self.frac_width, other.frac_width),
+                          signedness      = self.signedness,
+                          rounding_method = self.rounding_method,
+                          overflow_action = self.overflow_action)
 
     def __mul__(self, other: DeFixedInt) -> DeFixedInt:
-        ret = DeFixedInt(int_width     = self.int_width + other.int_width,
-                         frac_width    = self.frac_width + other.frac_width,
-                         signed_mode    = self.signed_mode,
-                         round_func    = self.round_func,
-                         saturate_func = self.saturate_func)
-        ret.value = self.fvalue * other.fvalue
-        return ret
+        return DeFixedInt(init_value      = self.fvalue * other.fvalue,
+                          int_width       = self.int_width + other.int_width,
+                          frac_width      = self.frac_width + other.frac_width,
+                          signedness      = self.signedness,
+                          rounding_method = self.rounding_method,
+                          overflow_action = self.overflow_action)
     
 if __name__ == '__main__':
 
-    a = DeFixedInt(2.05, 2, 2, round_func=round_to_even, saturate_func=saturate) 
-    b = DeFixedInt(2.05, 2, 4, round_func=round_to_even, saturate_func=saturate)
+    a = DeFixedInt(-223.14, 2, 2, signedness='U', rounding_method=round_half_to_even, overflow_action=saturate)
+    b = DeFixedInt(-2.05, 2, 4, signedness='U', rounding_method=round_half_to_even, overflow_action=wrap)
 
     print(a)
     print(b)
